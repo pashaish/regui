2691a694ffd6d33311fbcca450b547563992130f
pashaish <pashaishakov@gmail.com>
2021-11-01 22:40:38 +0300

    add json viewer

--- src/view/components/editor/hash.tsx
+++ src/view/components/editor/hash.tsx
@@ -6,6 +6,8 @@ import { colors, paddings } from '../../constants/colors';
 import { Split } from '../split';
 import { useDispatch } from 'react-redux';
 import { editorHashGetValue } from '../../actions/editor-hash';
+import ReactJson from 'react-json-view'
+
 
 const useStyles = createUseStyles({
     fieldsMenu: {
@@ -14,6 +16,10 @@ const useStyles = createUseStyles({
         height: '100%',
         overflow: 'overlay',
         boxShadow: `4px 0px 5px -4px ${colors.separator}`,
+    },
+    valueEditor: {
+        overflow: 'overlay',
+        height: '100%',
     },
     wrapper: {
         height: '100%'
@@ -25,6 +31,12 @@ export const HashEditor = () => {
     const styles = useStyles();
     const value = useSelector(store => store.editors.editorHashReducer.value);
     const dispatch = useDispatch();
+    let jsonValue: object | null;
+    try {
+        jsonValue = JSON.parse(value);
+    } catch (error) {
+        jsonValue = null;
+    }
 
     return <div className={styles.wrapper}>
         <Row>
@@ -36,7 +48,13 @@ export const HashEditor = () => {
                         </div>
                     })}
                 </div>
-                <div>{value}</div>
+                <div className={styles.valueEditor}>
+                    {
+                        jsonValue ?
+                            <ReactJson onEdit={() => {}} theme="twilight" style={{backgroundColor: 'transparent'}} src={jsonValue} /> :
+                            <div>{value}</div>
+                    }
+                </div>
             </Split>
         </Row>
     </div>;

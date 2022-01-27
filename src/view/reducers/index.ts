7d65f70617fad8c252d7f7cc6ed1eeec9865ccb6
pashaish <pashaishakov@gmail.com>
2022-01-27 13:36:29 +0300

    Fixes

--- src/view/reducers/index.ts
+++ src/view/reducers/index.ts
@@ -7,14 +7,6 @@ import { editorStringReducer } from './editor-string';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { useSelector as useSelectorDefault } from 'react-redux';
-import { composeWithDevTools } from 'remote-redux-devtools';
-
-const composeEnhancers = composeWithDevTools({
-    realtime: true,
-    port: 8000,
-    hostname: 'localhost',
-});
-
 
 export const store = createStore(
     combineReducers({
@@ -27,11 +19,9 @@ export const store = createStore(
             editorStringReducer,
         })
     }),
-    composeEnhancers(
-        applyMiddleware(
-            thunk,
-        )
-    ),
+    applyMiddleware(
+        thunk,
+    )
 )
 
 type selectorHookType = <R>(cb: (state: ReturnType<typeof store.getState>) => R) => R;
